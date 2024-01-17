f230d12153de56fea03fe73ed62a9cc4e60630ee
nidm <nidm@yahoo.com>
2024-01-17 15:12:08 -0800

    environment

---
+++ loopSysbench.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Pls modify accordingly
+ofile="sysBench_result"
+dbip="1.0.0.0"
+dbport="5432"
+dbuser="postgres"
+pw="notGoingToTellu"
+
+
+
+for ((i=2; i<=128; i=i*2)) 
+do 
+	echo "Thread = $i"
+	echo "######----- $(date) -----######" &>> "$ofile"
+	echo "Thread = $i" &>> "$ofile"
+	sysbench --db-driver=pgsql --pgsql-host="$dbip" --pgsql-port="$dbport" --pgsql-user="$dbuser" --pgsql-password="$pw"  --pgsql-db=t_sysbench --table_size=10000000 --tables=10 --events=0 --time=600  --threads=$i --percentile=95 --report-interval=60 oltp_read_write run &>> "$ofile"
+	sleep 20s
+done
